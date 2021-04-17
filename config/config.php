33f985a71ead344521710f81674cedf9654a35f9
SuiteResources <5281195+SuiteResources@users.noreply.github.com>
2021-04-17 12:47:54 -0500

    Config

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
     return [
-        SUITETALK_WSDL => ,
-        SUITETALK_CONSUMER_KEY => ,
-        SUITETALK_CONSUMER_SECRET => ,
-        SUITETALK_TOKEN_ID => ,
-        SUITETALK_TOKEN_SECRET => ,
-        SUITETALK_COMPANY_ID =>,
-        SUITETALK_NONCE_LENGTH => 20,
-        SUITETALK_VERSION => ,
-        SUITETALK_HOST => ,
-        SUITETALK_APPID =>
+        'wsdl' => env('SUITETALK_WSDL'),
+        'consumerkey' => env('SUITETALK_CONSUMER_KEY'),
+        'consumersecret' => env('SUITETALK_CONSUMER_SECRET'),
+        'tokenid' => env('SUITETALK_TOKEN_ID'),
+        'tokensecret' => env('SUITETALK_TOKEN_SECRET'),
+        'companyid' => env('SUITETALK_COMPANY_ID'),
+        'noncelength' => env('SUITETALK_NONCE_LENGTH'),
+        'host' => env('SUITETALK_HOST'),
+        'appid' => env('SUITETALK_APPID'),
+        'version' => env('SUITETALK_VERSION')
     ];
